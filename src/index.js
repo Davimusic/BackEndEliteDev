8db21b8f99bb1c44f305af0ec9c1441df8c06424
Jhon J. Ortiz <jjortiz016@misena.edu.co>
2021-11-27 23:41:51 -0500

    inicializando aplicacion node

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {ApolloServer} from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { Express } from 'express';
+import http from 'http'
+import dotenv from 'dotenv'
