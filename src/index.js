91635ef87cf6fee122a7bea3ffafffc66d5955a8
Jhon J. Ortiz <jjortiz016@misena.edu.co>
2021-11-29 22:08:51 -0500

    configuracion de servidor con datos de prueba

--- src/index.js
+++ src/index.js
@@ -1,5 +1,60 @@
-import {ApolloServer} from 'apollo-server-express';
+import { ApolloServer, gql } from 'apollo-server-express';
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
-import { Express } from 'express';
+import express from 'express';
 import http from 'http'
 import dotenv from 'dotenv'
+
+const typeDefs = gql`
+ 
+  type Book {
+    title: String
+    author: String
+  }
+
+  
+  type Query {
+    books: [Book]
+  }
+`;
+
+
+const books = [
+    {
+      title: 'The Awakening',
+      author: 'Kate Chopin',
+    },
+    {
+      title: 'City of Glass',
+      author: 'Paul Auster',
+    },
+  ];
+
+  const resolvers = {
+    Query: {
+      books: () => books,
+    },
+  };
+
+
+
+dotenv.config();
+
+const startApolloServer = async (typeDefs, resolvers) => { //typeDefs que en este caso es el schema y resolver
+    const app = express(); //creamos una app con express
+    const httpServer= http.createServer(app);
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    });
+
+    await server.start(); // para esperar que el servidor express inicie para poder aplicar el Middleware de la aplicación
+    server.applyMiddleware({ app }); 
+    await new Promise(resolve => httpServer.listen({port: process.env.PORT }, resolve));
+    console.log(`Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`);
+
+
+};
+startApolloServer(typeDefs,resolvers);
+
+
