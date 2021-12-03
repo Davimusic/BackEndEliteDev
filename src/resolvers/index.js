66fafba012673533cfef92bd029445fbf93a4070
Jhon J. Ortiz <jjortiz016@misena.edu.co>
2021-12-03 14:46:52 -0500

    se agrego resolver de user a index de resolver

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,15 +1,21 @@
 // qui utilizamo el express operator para clonar lo objetos que bienen del resolver
 import projectResolver from "./project.resolver.js";
+import userResolver from "./users.resolver.js";
 
 //const { Query: projectQueries, Mutation: ProjectMutatios, ...ProjectRest} = projectResolver;
 const { projectQueries, projectMutations, ...ProjectRest} = projectResolver; // se puede llamar asi si en el resolver se llaman igual
+const { userQueries, userMutations, ...UserRest} = userResolver;
+
 export default{
     Query:{
-        ...projectQueries
+        ...projectQueries,
+        ...userQueries
     },
     Mutation:{
-        ...projectMutations
+        ...projectMutations,
+        ...userMutations
     },
-    ...ProjectRest
+    ...ProjectRest,
+    ...UserRest
 };
 //00:44
